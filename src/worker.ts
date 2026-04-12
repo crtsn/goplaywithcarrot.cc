d653a2b014e06282fa5c42e3030957463000fd55
Name <@>
2026-04-12 20:34:07 +0200

    try serve static assets

--- src/worker.ts
+++ src/worker.ts
@@ -1,6 +1,5 @@
 interface Env {
-  MY_BUCKET: R2Bucket;
-  test: string;
+  ASSETS: Fetcher;
 }
 
 export default {
@@ -11,46 +10,59 @@ export default {
   ): Promise<Response> {
     const url = new URL(request.url);
     var only_image = false;
-    const r2 = env.MY_BUCKET;
-    const default_img = await r2.get('512x512_tile64.png');
     const isLocal = url.hostname.endsWith("localhost");
     const host = url.host;
+    const host_parts = url.host.split('.')
+    var baseHost = host;
+    var subdomain = null
+    if ((isLocal && host_parts.length == 2) || (host_parts.length == 3))
+    {
+      subdomain = host_parts[0]
+      baseHost = host_parts.slice(1).join('.')
+    }
 
     // for (let key in request) {
     //   console.log(key, request[key])
     // }
     // console.log([...request.headers]);
 
-    console.log("URL: " + url);
-    console.log("HOST: " + host);
     let user_agent = request.headers.get("user-agent");
     only_image = user_agent && user_agent.includes("Discordbot");
     only_image = only_image || (url.searchParams.get("i") != null);
 
-    console.log(`only_image: ${only_image}`)
     if (only_image)
     {
-      if (default_img) {
-        return new Response(default_img.body, {
-          headers: {
-            'Content-Type': default_img.httpMetadata.contentType || 'image/png',
-            'Cache-Control': 'public, max-age=3600',
-          },
-        });
+      let img_url;
+
+      switch(subdomain) {
+        case "xn--4o8h":
+          img_url = "img/rabbit.png";
+          break;
+        case "xn--yn8h":
+          img_url = "img/rabbit2.png";
+          break;
+        case "xn--dp8h":
+          img_url = "img/frog.png";
+          break;
+        default:
+          img_url = "img/hedgehog.png";
       }
+      let img_request = new Request(`http://${baseHost}/${img_url}`);
+      const assetResponse = await env.ASSETS.fetch(img_request);
+      return assetResponse;
     }
 
     const html = `<!DOCTYPE html>
     <body>
-      <a href="http://${host}">${host}</a>
+      <a href="http://${baseHost}">${baseHost}</a>
       <p>URL used: ${url}</p>
       <div style="display: flex; align-items: center;">
         <h1>Meet players:</h1>
         <div style="display: flex; align-items: center;">
-          <a href="http://\u{1F430}.${host}">\u{1F430}</a>
-          <a href="http://\u{1F407}.${host}">\u{1F407}</a>
-          <a href="http://\u{1F438}.${host}">\u{1F438}</a>
-          <a href="http://\u{1F994}.${host}">\u{1F994}</a>
+          <a href="http://\u{1F430}.${baseHost}">\u{1F430}</a>
+          <a href="http://\u{1F407}.${baseHost}">\u{1F407}</a>
+          <a href="http://\u{1F438}.${baseHost}">\u{1F438}</a>
+          <a href="http://\u{1F994}.${baseHost}">\u{1F994}</a>
         </div>
       </div>
 	    <img src="?i=1">
