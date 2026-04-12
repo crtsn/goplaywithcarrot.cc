9eadf86f22cfc455ec6534f0e3ac93f9393ccf93
Name <@>
2026-04-12 19:16:20 +0200

    Tested host on localhost and got weird links, will it break on live instanse also?

--- src/worker.ts
+++ src/worker.ts
@@ -13,12 +13,21 @@ export default {
     var only_image = false;
     const r2 = env.MY_BUCKET;
     const default_img = await r2.get('512x512_tile64.png');
+    const isLocal = url.hostname.endsWith("localhost");
+    const host = url.host;
+
+    // for (let key in request) {
+    //   console.log(key, request[key])
+    // }
+    // console.log([...request.headers]);
 
     console.log("URL: " + url);
+    console.log("HOST: " + host);
     let user_agent = request.headers.get("user-agent");
     only_image = user_agent && user_agent.includes("Discordbot");
     only_image = only_image || (url.searchParams.get("i") != null);
 
+    console.log(`only_image: ${only_image}`)
     if (only_image)
     {
       if (default_img) {
@@ -33,15 +42,15 @@ export default {
 
     const html = `<!DOCTYPE html>
     <body>
-      <a href="http://goplaywithcarrot.cc">goplaywithcarrot.cc</a>
+      <a href="http://${host}">${host}</a>
       <p>URL used: ${url}</p>
       <div style="display: flex; align-items: center;">
         <h1>Meet players:</h1>
         <div style="display: flex; align-items: center;">
-          <a href="http://\u{1F430}.goplaywithcarrot.cc">\u{1F430}</a>
-          <a href="http://\u{1F407}.goplaywithcarrot.cc">\u{1F407}</a>
-          <a href="http://\u{1F438}.goplaywithcarrot.cc">\u{1F438}</a>
-          <a href="http://\u{1F994}.goplaywithcarrot.cc">\u{1F994}</a>
+          <a href="http://\u{1F430}.${host}">\u{1F430}</a>
+          <a href="http://\u{1F407}.${host}">\u{1F407}</a>
+          <a href="http://\u{1F438}.${host}">\u{1F438}</a>
+          <a href="http://\u{1F994}.${host}">\u{1F994}</a>
         </div>
       </div>
 	    <img src="?i=1">
